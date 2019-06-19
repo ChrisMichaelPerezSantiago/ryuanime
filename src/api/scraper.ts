c24e7648e2c9ab6058748b80b047dba1960e2773
Chris Michael <chris.perez@upr.edu>
2019-06-19 15:25:24 -0400

    :sparkles: it has become possible to get the mp4 videos using the package [puppeteer]

--- src/api/scraper.ts
+++ src/api/scraper.ts
@@ -1,13 +1,15 @@
 import * as cheerio from 'cheerio';
+import * as puppeteer from 'puppeteer';
 import * as jsdom from 'jsdom';
 import fetch from 'node-fetch';
 import { url, searchUrl, searchUrlLetter } from './urls/index'
 import IAnime from './interfaces/IAnime'
 
+
 const { JSDOM } = jsdom;
 
-const getAnimesListByLetter = async (letter: string) => {
-  const res = await fetch(`${searchUrlLetter}${letter}`);
+const getAnimesListByLetter = async (letter: string, page: number) => {
+  const res = await fetch(`${searchUrlLetter}${letter}/${page}`);
   const body = await res.text();
   const animes = [];
   const $ = cheerio.load(body);
@@ -44,13 +46,14 @@ const searchAnime = async (query: any) => {
     const type = $element.find('span.eps-num').text();
     const id = $element.find('a.let-link').attr('href').split('/')[3];
     const poster = $element.find('a').children('img').attr('src');
-    const extra = getAnimeVideo(id, undefined);
+    //const extra = getAnimeVideo(id, undefined);
+    //console.log(extra);
     const anime: IAnime[] = [{
       title: title,
       id: id,
       poster: poster,
       type: type,
-      extra: extra,
+      extra: 'extra',
       synopsis: synopsis,
     }]
     animes.push(anime);
@@ -58,68 +61,29 @@ const searchAnime = async (query: any) => {
   return animes;
 }
 
-// from devtool
-//document.querySelector("iframe.player_conte").contentDocument.body.querySelector("source").src
 
 const getAnimeVideo = async (id: string, chapter: number) => {
-  const res = await fetch(`${url}${id}/${chapter}/`);
-  const body = await res.text();
-  //const options = {
-  //  resources: 'usable',
-  //  runScripts: 'dangerously',
-  //};
-  //JSDOM.fromFile(body , options).then((dom) =>{
-  //  console.log("info => " , dom.window.document.body.querySelector('iframe.player_conte').textContent.trim());
-  //}).catch((err) => console.log(err))
-  //const content = [];
-  //const $ = cheerio.load(body);
-  //const dom = new JSDOM(body);
-  //const video = dom.window.document.querySelector("iframe")
-  //$(video).each(function(index , element){
-  //  const $element = $(element).attr('src')
-  //  const source = {
-  //    video: $element
-  //  };
-  //  content.push(source);
-  //});
-  //console.log("content => " , content);
-  //return {body};
-  //const $ = cheerio.load(body);
-  //$('div#container').each((index , element) =>{
-  //  const $element = $(element).text();
-  //  const iframe = $('iframe').attr('src')
-  //  const $$ = cheerio.load(iframe);
-  //  $$('iframe.player_conte').each((i , e) =>{
-  //    const $$element = $$(e).text();
-  //    console.log($$element);
-  //  })
-  //})
-  //const options = {
-  //  resources: 'usable',
-  //  runScripts: 'dangerously',
-  //};
-  //JSDOM.fromFile(body, options).then((dom) => {
-  //  const $ = cheerio.load(body);
-  //  let iframe = dom.window.document.body.querySelector('iframe').textContent.trim();
-  //    $(`${iframe}`).each(function (index, element) {
-  //      const $element = $(element);
-  //      const x = $element.find('#player_conte source').attr('src')
-  //      console.log(x);
-  //    })
-  //})
-  //const $ = cheerio.load(body);
-  //const arr = []
-  //let x = $('iframe').contents().find('html').html();
-  //console.log(x)
-  //
-  //$('iframe').each((index, element) => {
-  //  const $element = $(element);
-  //  const x = $element.attr('src');
-  //  //console.log(x);
-  //  arr.push(x);
-  //});
-  //return {}; 
+  const BASE_URL = `${url}${id}/${chapter}/`;
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(BASE_URL);
+  const elementHandle = await page.$('.player_conte');
+  const frame = await elementHandle.contentFrame();
+  const video = await frame.$eval('#jkvideo_html5_api', el =>
+    Array.from(el.getElementsByTagName('source')).map(e => e.getAttribute("src")));
+  return video;
 }
+
+
+// Testing Testing Asynchronous Code
+(async () => {
+  const T = await getAnimeVideo('tokyo-magnitude-80', 1)
+    .then(async (data) => {
+      return await data;
+    })
+  console.log(T)
+})()
+
 
 export {
   searchAnime,
