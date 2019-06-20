a119c0e425ad3c826b37bd3e686433b0a9e015f9
Chris Michael <chris.perez@upr.edu>
2019-06-20 15:14:49 -0400

    :fire: extra property from the animes array has been removed.

--- src/api/scraper.ts
+++ src/api/scraper.ts
@@ -1,12 +1,10 @@
 import * as cheerio from 'cheerio';
 import * as puppeteer from 'puppeteer';
-import * as jsdom from 'jsdom';
 import fetch from 'node-fetch';
 import { url, searchUrl, searchUrlLetter } from './urls/index'
 import IAnime from './interfaces/IAnime'
 
 
-const { JSDOM } = jsdom;
 
 const lastAnimesAdded = async () =>{
   const res = await fetch(`${url}`);
@@ -20,13 +18,12 @@ const lastAnimesAdded = async () =>{
     //const type = $element.find('span.eps-num').text();
     const id = $element.find('h2.portada-title a').attr('href').split('/')[3];
     const poster = $element.find('a').children('img').attr('src');
-    const extra = getAnimeVideo(id, undefined);
+    //const extra = getAnimeVideo(id, undefined);
     const anime: IAnime[] = [{
       title: title,
       id: id,
       poster: poster,
       type: 'unknown',
-      extra: extra,
       synopsis: 'unknown',
     }]
     animes.push(anime);
@@ -46,13 +43,11 @@ const getAnimesListByLetter = async (letter: string, page: number) => {
     const type = $element.find('span.eps-num').text();
     const id = $element.find('a.let-link').attr('href').split('/')[3];
     const poster = $element.find('a').children('img').attr('src');
-    const extra = getAnimeVideo(id, undefined);
     const anime: IAnime[] = [{
       title: title,
       id: id,
       poster: poster,
       type: type,
-      extra: extra,
       synopsis: synopsis,
     }]
     animes.push(anime);
@@ -65,21 +60,18 @@ const searchAnime = async (query: any) => {
   const body = await res.text();
   const animes = [];
   const $ = cheerio.load(body);
-  $('.portada-box').each(function (index, element) {
+  $('.portada-box').each(async function(index, element){
     const $element = $(element);
     const title = $element.find('h2.portada-title a').attr('title');
     const synopsis = $element.find('div #ainfo p').text();
     const type = $element.find('span.eps-num').text();
     const id = $element.find('a.let-link').attr('href').split('/')[3];
     const poster = $element.find('a').children('img').attr('src');
-    //const extra = getAnimeVideo(id, undefined);
-    //console.log(extra);
     const anime: IAnime[] = [{
       title: title,
       id: id,
       poster: poster,
       type: type,
-      extra: 'extra',
       synopsis: synopsis,
     }]
     animes.push(anime);
@@ -101,20 +93,34 @@ const getAnimeVideo = async (id: string, chapter: number) => {
   return video;
 }
 
-
-// Testing Testing Asynchronous Code
-//(async () => {
-//  const T = await getAnimeVideo('tokyo-magnitude-80', 1)
-//    .then(async (data) => {
-//      return await data;
-//    })
-//  console.log(T)
+// Video function helper!! Testing 
+//async function getVideoHelper(id: string, chapter: number){
+//  console.log('id: ' , id , 'chapater: ' , chapter);
+//  try{
+//    var T = await getAnimeVideo(id, chapter)
+//    await delay();
+//    return await JSON.stringify(T);
+//  }catch(err){
+//    return err;
+//  }
+//}
+//
+//(async function(){
+//  var T = await getVideoHelper('tokyo-ghoul', 1)
+//  console.log(T);
 //})()
-
+//
+//function delay(){
+//  return new Promise(function(resolve , reject){
+//    setTimeout(function(){
+//      resolve(42)
+//    }, 5000)
+//  });
+//}
 
 export {
   searchAnime,
   getAnimeVideo,
   getAnimesListByLetter,
-  lastAnimesAdded
+  lastAnimesAdded,
 };
