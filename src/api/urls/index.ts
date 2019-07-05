d42dc9e54ac892b7ea6255520ef0dfccbdff4d16
Chris Michael <chris.perez@upr.edu>
2019-07-05 17:32:41 -0400

    The url of the jkanime calendar has been added.

--- src/api/urls/index.ts
+++ src/api/urls/index.ts
@@ -1,4 +1,4 @@
-
   export const url: String = 'https://jkanime.net/';
   export const searchUrl: String = 'https://jkanime.net/buscar/';
   export const searchUrlLetter: String = 'https://jkanime.net/letra/';
+  export const calenderUrl: String = 'https://jkanime.net/horario/';
