7f9e71a8c45dda2e9a87650e3a3bdc666b3a7e9f
Chris Michael <chris.perez@upr.edu>
2019-07-07 18:50:04 -0400

    minor changes done

--- src/frontend/src/router.js
+++ src/frontend/src/router.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import LatestAnimeVideo from "./views/LatestAnimeVideo"
-import AnimesByLetter from "./views/AnimesByLetter"
-import AnimesByGender from "./views/AnimesByGender"
-import SearchAnime from "./views/SearchAnime"
-import NotFound from "./views/NotFound"
+import LatestAnimeVideo from "./views/LatestAnimeVideo";
+import AnimesByLetter from "./views/AnimesByLetter";
+import AnimesByGender from "./views/AnimesByGender";
+import SearchAnime from "./views/SearchAnime";
+import NotFound from "./views/NotFound";
 
 
 Vue.use(Router);
