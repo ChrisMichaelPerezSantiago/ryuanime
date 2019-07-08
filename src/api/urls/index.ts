02ce131285e2ac55c2375de7159a1be89cd41f1a
Chris Michael <chris.perez@upr.edu>
2019-07-07 21:41:28 -0400

    url of the ova section was added.

--- src/api/urls/index.ts
+++ src/api/urls/index.ts
@@ -3,4 +3,5 @@
   export const searchUrlLetter: String = 'https://jkanime.net/letra/';
   export const calenderUrl: String = 'https://jkanime.net/horario/';
   export const genderUrl: String = 'https://jkanime.net/genero/';
-  export const moviesUrl: String = 'https://jkanime.net/tipo/pelicula'
+  export const moviesUrl: String = 'https://jkanime.net/tipo/pelicula';
+  export const ovasUrl: String = 'https://jkanime.net/tipo/ova';
