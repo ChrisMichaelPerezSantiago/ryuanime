c05efd5966d18d7ec5819a24c58c441585dd0abe
Chris Michael <chris.perez@upr.edu>
2019-07-07 20:31:59 -0400

    link to the movies section and OVAs was added.

--- src/frontend/src/views/NavBar.vue
+++ src/frontend/src/views/NavBar.vue
@@ -47,6 +47,24 @@
                <router-link to="/AnimesByGender"><span class="text-white opacity-75">ლ Gender</span></router-link>
            </div>
        </div>
+       <div class="mb-8">
+          <div class="px-4 mb-2 text-white flex justify-between items-center">
+            <div class="opacity-75">♝ By Type </div>
+            <div>
+                <svg class="fill-current h-4 w-4 opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z" />
+                </svg>
+            </div>
+          </div>
+           <div class="flex items-center mb-3 px-4">
+               <span class="bg-green rounded-full block w-2 h-2 mr-2"></span>
+               <router-link to="/Movies"><span class="text-white opacity-75">シ Movies</span></router-link>
+           </div>
+           <div class="flex items-center mb-3 px-4">
+               <span class="bg-green rounded-full block w-2 h-2 mr-2"></span>
+               <router-link to="/Ovas"><span class="text-white opacity-75">シ Ovas</span></router-link>
+           </div>
+       </div>
   </div>
 </template>
 
