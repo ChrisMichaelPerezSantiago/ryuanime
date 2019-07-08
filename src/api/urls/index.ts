e982a64af5d22c0f0b07b71a6cf9fa3ed4237a5b
Chris Michael <chris.perez@upr.edu>
2019-07-07 20:30:12 -0400

    url of the movies section was added.

--- src/api/urls/index.ts
+++ src/api/urls/index.ts
@@ -2,4 +2,5 @@
   export const searchUrl: String = 'https://jkanime.net/buscar/';
   export const searchUrlLetter: String = 'https://jkanime.net/letra/';
   export const calenderUrl: String = 'https://jkanime.net/horario/';
-  export const genderUrl: String = 'https://jkanime.net/genero/'
+  export const genderUrl: String = 'https://jkanime.net/genero/';
+  export const moviesUrl: String = 'https://jkanime.net/tipo/pelicula'
