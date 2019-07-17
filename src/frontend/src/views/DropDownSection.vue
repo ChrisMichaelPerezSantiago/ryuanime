868bb861a24faf4c993a641f7fe777a815c3efd7
Chris Michael <chris.perez@upr.edu>
2019-07-17 17:24:43 -0400

    DropDownSelection.vue template create

---
+++ src/frontend/src/views/DropDownSection.vue
@@ -0,0 +1,38 @@
+<template>
+  <select class="list-reset border border-purple-200 rounded w-20 font-sans" v-model="to">
+    <option v-for="(option , index) in options"
+      :value="option"
+      :key="index"
+      >
+      {{ option.section }}
+    </option>
+  </select>
+</template>
+
+
+<script>
+  export default {
+    components: {
+      Multiselect
+    },
+    data(){
+      return{
+        to: [],
+        options: [
+          { section: "Latest"   ,  to: "/" },
+          { section: "Search"   ,  to: "/SearchAnime" },
+          { section: "Alphabet" ,  to: "/AnimesByLetter" },
+          { section: "Genders"  ,  to: "/AnimesByGender" },
+          { section: "Movies"   ,  to: "/Movies" },
+          { section: "Ovas"     ,  to: "/Ovas"}
+        ]
+      }
+    },
+    watch:{
+      "to": function(value){
+        let go = value;
+        this.$router.push(go.to);
+      }
+    }
+  };
+</script>
