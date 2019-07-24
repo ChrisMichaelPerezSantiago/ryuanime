7fefca4fd7613dc3eda0a19676fb8ba065e645c4
Chris Michael <chris.perez@upr.edu>
2019-07-24 16:53:04 -0400

    electron app height fixed

--- src/frontend/src/background.js
+++ src/frontend/src/background.js
@@ -16,7 +16,7 @@ protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 1200, height: 700, webPreferences: {
+  win = new BrowserWindow({ width: 1500, height: 800, webPreferences: {
     nodeIntegration: true
   } })
 
