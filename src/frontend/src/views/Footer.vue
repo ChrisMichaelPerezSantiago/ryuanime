0a91d97173a7822d4fe39b107ebbd07f3995f1f7
Chris Michael <chris.perez@upr.edu>
2019-08-07 10:27:20 -0400

    ryuanime to Ryuanime fixed

--- src/frontend/src/views/Footer.vue
+++ src/frontend/src/views/Footer.vue
@@ -3,7 +3,7 @@
     <div class="max-w-md">
       <div class="bg-blue text-white font-bold py-2 px-4 mx-auto block rounded"  id="js-load-map"></div>
       <p class="text-sm text-grey-dark flex items-start py-2 leading-normal">
-        <span>© {{year}} ryuanime , All rights reserved</span>
+        <span>© {{year}} Ryuanime , All rights reserved</span>
       </p>
     </div>
   </div>
