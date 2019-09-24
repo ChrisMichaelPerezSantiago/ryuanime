1b03cd0818cb1eaa672e000c69cb7f1d1a34598f
Chris Michael <chris.perez@upr.edu>
2019-09-24 09:59:44 -0400

    File paths were modified inside folder v1

--- src/api/endpoints.controller.ts
+++ src/api/endpoints.controller.ts
@@ -1,11 +1,11 @@
-import * as scraper from './scraper/scraper';
+import * as V1_API from './v1/scraper/scraper';
 import * as express from 'express';
 const router = express.Router();
 
 
 router.get('/search/:title', (req: express.Request, res: express.Response) => {
   const title: string = req.params.title;
-  scraper.searchAnime(title)
+  V1_API.searchAnime(title)
     .then(animes => {
       res.status(200).json({
         animes
@@ -18,7 +18,7 @@ router.get('/search/:title', (req: express.Request, res: express.Response) => {
 router.get('/anime/:id/:chapter', (req: express.Request, res: express.Response) => {
   const id: string = req.params.id;
   const chapter: number = req.params.chapter;
-  scraper.getAnimeVideo(id, chapter)
+  V1_API.getAnimeVideo(id, chapter)
     .then(video => {
       res.status(200).json({
         video
@@ -31,7 +31,7 @@ router.get('/anime/:id/:chapter', (req: express.Request, res: express.Response) 
 router.get('/letter/:letter/:page', (req: express.Request, res: express.Response) => {
   const letter: string = req.params.letter.toUpperCase();
   const page: number = req.params.page;
-  scraper.getAnimesListByLetter(letter , page)
+  V1_API.getAnimesListByLetter(letter , page)
     .then(animes => {
       res.status(200).json({
         animes
@@ -43,7 +43,7 @@ router.get('/letter/:letter/:page', (req: express.Request, res: express.Response
 
 
 router.get('/latest', (req: express.Request, res: express.Response) => {
-  scraper.lastAnimesAdded()
+  V1_API.lastAnimesAdded()
     .then(animes => {
       res.status(200).json({
         animes
@@ -54,7 +54,7 @@ router.get('/latest', (req: express.Request, res: express.Response) => {
 });
 
 router.get('/calender' , (req: express.Request , res: express.Response) =>{
-  scraper.getAnimeCalender()
+  V1_API.getAnimeCalender()
     .then(calender =>{
       res.status(200).json({
         calender
@@ -66,7 +66,7 @@ router.get('/calender' , (req: express.Request , res: express.Response) =>{
 router.get('/genders/:gender/:page' , (req: express.Request , res: express.Response) =>{
   let gender: string = req.params.gender.toLowerCase();
   let page: number = req.params.page;
-  scraper.getAnimesByGender(gender , page)
+  V1_API.getAnimesByGender(gender , page)
     .then(animes =>{
       res.status(200).json({
         animes
@@ -76,7 +76,7 @@ router.get('/genders/:gender/:page' , (req: express.Request , res: express.Respo
 
 router.get('/movies/:page' , (req: express.Request , res: express.Response) =>{
   let page: number = req.params.page;
-  scraper.getAnimeMovies(page)
+  V1_API.getAnimeMovies(page)
     .then(movies =>{
       res.status(200).json({
         movies
@@ -86,7 +86,7 @@ router.get('/movies/:page' , (req: express.Request , res: express.Response) =>{
 
 router.get('/ovas/:page' , (req: express.Request , res: express.Response) =>{
   let page: number = req.params.page;
-  scraper.getAnimeOvas(page)
+  V1_API.getAnimeOvas(page)
     .then(ovas =>{
       res.status(200).json({
         ovas
